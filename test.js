b2aac3013721d4d3a4c2001c175fe6dbfb75eddc
raininfotech-sagar184 <168505762+raininfotech-sagar184@users.noreply.github.com>
2024-06-06 11:48:04 +0530

    sv || 19. Singly liked list

--- test.js
+++ test.js
@@ -1,18 +1,36 @@
-function getDigit(num, i) {
-  return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
-}
-function digitCount(num) {
-  if (num === 0) return 1;
-  return Math.floor(Math.log10(Math.abs(num))) + 1;
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
 }
 
-function mostDigits(nums) {
-  let maxDigits = 0;
-  for (let i = 0; i < nums.length; i++) {
-    maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
-  return maxDigits;
+  push(val) {
+    var newNode = new Node(val);
+    if (!this.head) {
+      console.log("head newNode")
+
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      console.log("this.head", this.head)
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
-console.log(digitCount(100, 0))
-console.log(digitCount(10,))
-console.log(digitCount(101,)) 
+
+var list = new SinglyLinkedList()
+list.push("HELLO")
+list.push("GOODBYE")
+list.push("RAIN")
+list.push("infotech")
+// console.log(list)
