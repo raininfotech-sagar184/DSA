c2cdd373ed4dd1a5a6f0da4e0f9f50cd73d7568b
raininfotech-sagar184 <168505762+raininfotech-sagar184@users.noreply.github.com>
2024-06-07 12:06:25 +0530

    sv | linked list

--- test.js
+++ test.js
@@ -1,36 +1,38 @@
-class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
+class BrowserHistory {
+  constructor(homepage) {
+    this.history = [homepage]; // Array to store history
+    this.currentIndex = 0;     // Index to keep track of the current page
+  }
+
+  visit(url) {
+    // Remove all forward history
+    this.history = this.history.slice(0, this.currentIndex + 1);
+    // Add the new url
+    this.history.push(url);
+    // Move the current index to the new page
+    this.currentIndex++;
+  }
+
+  back(steps) {
+    // Move back up to 'steps' times or to the start of the history
+    this.currentIndex = Math.max(0, this.currentIndex - steps);
+    return this.history[this.currentIndex];
+  }
+
+  forward(steps) {
+    // Move forward up to 'steps' times or to the end of the history
+    this.currentIndex = Math.min(this.history.length - 1, this.currentIndex + steps);
+    return this.history[this.currentIndex];
   }
 }
 
-class SinglyLinkedList {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
-  }
-  push(val) {
-    var newNode = new Node(val);
-    if (!this.head) {
-      console.log("head newNode")
-
-      this.head = newNode;
-      this.tail = this.head;
-    } else {
-      console.log("this.head", this.head)
-      this.tail.next = newNode;
-      this.tail = newNode;
-    }
-    this.length++;
-    return this;
-  }
-}
-
-var list = new SinglyLinkedList()
-list.push("HELLO")
-list.push("GOODBYE")
-list.push("RAIN")
-list.push("infotech")
-// console.log(list)
+// Example usage:
+const browserHistory = new BrowserHistory('homepage');
+browserHistory.visit('page1');        // Visit 'page1'
+browserHistory.visit('page2');        // Visit 'page2'
+console.log(browserHistory.back(1));  // Go back to 'page1', prints 'page1'
+console.log(browserHistory.back(1));  // Go back to 'homepage', prints 'homepage'
+console.log(browserHistory.forward(1)); // Go forward to 'page1', prints 'page1'
+browserHistory.visit('page3');        // Visit 'page3', erasing forward history
+console.log(browserHistory.back(2));  // Go back to 'homepage', prints 'homepage'
+console.log(browserHistory.forward(2)); // Go forward to 'page3', prints 'page3'
